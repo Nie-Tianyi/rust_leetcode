932dc8f28836e131701a0c4975590769a3943aa4
nty-mac <1355705558@qq..com>
2024-06-19 00:07:49 +0800

    add binary search

--- src/basics/double_linked_list.rs
+++ src/basics/double_linked_list.rs
@@ -25,26 +25,6 @@ impl DoubleLinkedList {
         }
     }
 
-    /// 从给定位置插入一个值
-    pub fn insert(&mut self, index: usize, val: i32) -> Result<(),&str> {
-        if index > self.len{
-            return Err("invalid index")
-        }
-        // 初始化指向头节点的指针
-        let mut current_node = &mut self.head_node;
-        // 移动指针
-        for _ in 0..index {
-            current_node = current_node.next_node.as_mut().unwrap();
-        }
-        let new_node = Node {
-            prev_node: Some(Box::new(current_node)),
-            next_node: current_node.next_node.take(),
-            val,
-        };
-        current_node.next_node = Some(Box::new(new_node));
-        Ok(())
-    }
-
     pub fn delete(&mut self, index: usize, val: i32) -> Result<(),&str>  {
         unimplemented!()
     }
