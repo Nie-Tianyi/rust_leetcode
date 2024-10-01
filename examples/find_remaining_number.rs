904dd671a35b250fdb32f673935456c608eae281
nty-mac <1355705558@qq..com>
2024-10-01 19:52:02 +0800

    feat: add find_remaining_number.rs and unrepeated_string.rs

---
+++ examples/find_remaining_number.rs
@@ -0,0 +1,46 @@
+use std::io;
+use std::io::BufRead;
+/// 有一个数组 a[N] 顺序存放 0 ~ N-1 ，要求每隔两个数删掉一个数，
+/// 到末尾时循环至开头继续进行，求最后一个被删掉的数的原始下标位置。
+/// 以 8 个数 (N=7) 为例 :｛ 0，1，2，3，4，5，6，7 ｝，0 -> 1 -> 2 (删除)
+/// -> 3 -> 4 -> 5 (删除) -> 6 -> 7 -> 0 (删除),如此循环直到
+/// 最后一个数被删除。
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let num = lines.next()
+        .unwrap_or(Ok("0".to_string()))
+        .unwrap_or("0".to_string())
+        .parse::<usize>()
+        .unwrap_or(0);
+
+    let num = find_last(num);
+
+    println!("{}", num)
+}
+
+fn find_last(n: usize) -> usize {
+    if n <= 1 {
+        return 0;
+    }
+    let mut v = Vec::with_capacity(n);
+    for i in 0..n {
+        v.push(i);
+    }
+    pop_elements(&mut v)
+}
+
+// 3,6
+fn pop_elements(v: &mut Vec<usize>) -> usize {
+    let mut index = 0;
+    let mut res = 0;
+    loop {
+        if v.len() <=2 {
+            return res;
+        }
+        if (index + 1) % 3 == 0 {
+            res = v.remove(index % v.len());
+        }
+        index += 1;
+    }
+}
---
+++ examples/unrepeated_string.rs
@@ -0,0 +1,15 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let mut lines = io::stdin().lock().lines();
+    let input = lines.next().unwrap().unwrap();
+    let mut res = String::new();
+    for ch in input.chars() {
+        if !res.contains(ch) {
+            res.push(ch);
+        }
+    }
+    println!("{}",res)
+}
+
