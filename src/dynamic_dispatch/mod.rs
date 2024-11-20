eab95ae26c3aee1b4329a55dc62ac104a24c0756
nty-mac <1355705558@qq..com>
2024-11-20 18:09:58 +0800

    amend comments in dynamic_dispatch/mod.rs

--- src/dynamic_dispatch/mod.rs
+++ src/dynamic_dispatch/mod.rs
@@ -5,6 +5,8 @@
 /// 动态规划就是 “iterate with memory”，即带有记忆的遍历搜索所有可能的情况。
 /// 在遍历的过程中，我们可以缓存一些子问题的结果，这样就不用每次遇到该子问题都需要去重新计算。
 ///
+/// 通常我们使用一个数组或者二维数组缓存这一系列中间子问题结果，通常把其称为dp数组。
+///
 /// ### 解决动态规划问题需要注意的关键点
 ///
 /// 1. dp数组的下标以及其含义
@@ -14,6 +16,7 @@
 /// 5. 打印二维数组 Debug
 mod climbing_stairs;
 mod fibonacci_number;
+mod integer_break;
 mod min_cost_climbing_stairs;
 mod unique_paths;
 mod unique_paths_2;
