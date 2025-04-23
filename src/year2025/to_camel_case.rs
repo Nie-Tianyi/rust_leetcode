61f965383fea9828387d3b756a4e312c4c2d2d60
nty-pc <1355705558@qq.com>
2025-04-23 16:57:20 +0800

    feat: add to_camel_case.rs

--- src/year2025/to_camel_case.rs
+++ src/year2025/to_camel_case.rs
@@ -13,7 +13,7 @@ fn to_camel_case(text: &str) -> String {
                 } else {
                     res.push(c);
                 }
-                start_of_a_word = false
+                start_of_a_word = false;
             }
             _ => {
                 res.push(c);
@@ -31,17 +31,17 @@ mod tests {
 
     const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
 
-    fn dotest(s: &str, expected: &str) {
+    fn do_test(s: &str, expected: &str) {
         assert_eq!(to_camel_case(s), expected, "{ERR_MSG} with text = \"{s}\"")
     }
 
     #[test]
     fn fixed_tests() {
-        dotest("", "");
-        dotest("the_stealth_warrior", "theStealthWarrior");
-        dotest("The-Stealth-Warrior", "TheStealthWarrior");
-        dotest("A-B-C", "ABC");
-        dotest("this is a test", "this Is A Test");
-        dotest("这个是测试 test", "这个是测试 Test");
+        do_test("", "");
+        do_test("the_stealth_warrior", "theStealthWarrior");
+        do_test("The-Stealth-Warrior", "TheStealthWarrior");
+        do_test("A-B-C", "ABC");
+        do_test("this is a test", "this Is A Test");
+        do_test("这个是测试 test", "这个是测试 Test");
     }
 }
