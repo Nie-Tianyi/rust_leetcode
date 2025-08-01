c7ef31ee165c796b84d2739d53bc2d56ce0d4a2b
nty-pc <1355705558@qq.com>
2025-08-01 21:39:21 +0800

    feat: add largest_number.rs

--- src/year2025/largest_number.rs
+++ src/year2025/largest_number.rs
@@ -1,26 +1,38 @@
-use std::cmp::Ordering;
-
 struct Solution;
 
 impl Solution {
     #[allow(unused)]
     pub fn largest_number(nums: Vec<i32>) -> String {
-        let mut nums = nums;
+        let mut nums: Vec<String> = nums.iter().map(|a| a.to_string()).collect();
         // 排序，按照从左到右数字大小排序
-        nums.sort_by(|a, b| {
-            unimplemented!()
-        });
+        nums.sort_by(|a, b| (a.clone() + b).cmp(&(b.clone() + a)));
 
         nums.reverse();
-        let nums: Vec<String> = nums.iter().map(|a| a.to_string()).collect();
-        
-        nums.concat()
+
+        let mut res = nums.concat();
+
+        remove_leading_zeros(&mut res);
+
+        res
+    }
+}
+
+fn remove_leading_zeros(s: &mut String) {
+    while s.starts_with("0") && s.len() > 1 {
+        s.drain(..1);   
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Solution;
+    use super::{remove_leading_zeros, Solution};
+
+    #[test]
+    fn test_remove() {
+        let mut s = String::from("0001");
+        remove_leading_zeros(&mut s);
+        println!("{s}")
+    }
 
     #[test]
     fn test_1() {
@@ -33,10 +45,10 @@ mod tests {
         let nums = vec![3, 30, 34, 5, 9];
         assert_eq!(Solution::largest_number(nums), String::from("9534330"));
     }
-    
+
     #[test]
     fn test_3() {
-        let nums = vec![34323,3432];
-        assert_eq!(Solution::largest_number(nums), String::from("343233432"));
+        let nums = vec![34323, 3432];
+        assert_eq!(Solution::largest_number(nums), String::from("343234323"));
     }
 }
